262230ffb9f6dfac5a3cc4582024a32b994fcef1
TheCodedHuman <mokshmahawar05@gmail.com>
2025-03-06 17:37:25 +0000

    variables and datatypes

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,23 @@
+// initial values
+
+const accountId = 144553
+let accountEmail = "moksh@2025.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2 // not allowed
+let accountState;
+
+// Secondary Values
+accountEmail = "mm@mm.com"
+accountPassword = "5050505050"
+accountCity = "Bhopal"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+// Main
+console.log(accountState);
+// console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js and not browser (works in browser console)
+
+console.log(3
+    +
+    3
+);          // avoid using this; improve code readability
+
+//
+
+console.log("Jai Jai")
+
+
+let name = "MM"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+/*
+number 2 to power^53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+*/
+
+// we can find the type of data by typeof
+console.log(typeof state);
+console.log(typeof null)
+console.log(typeof undefined)
